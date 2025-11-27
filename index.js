077b463c00a72f8cb1768851d70f9b16d787937c
telmanisgenderov182-prog <telmanisgenderov182@gmail.com>
2025-11-27 18:57:16 +0400

    uu

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let username = "Telman"
-let surname = "isgenderov"
-console.log( " sizi birdaha gormeyimize sadiq," + " " + username + " " + surname + " .")
+// birinci tapsiriq
+let fullName  = "Telman Isgenderov";
+let parts = fullName.split(" ");
+let firstInital = parts[0] [0];
+let lastInital = parts[1] [0];
+console.log(firstInital + lastInital);
+
+
+// ikinci taapsiriq
+let fullName1 = "Telman Isgenderov";
+let nameParts = fullName1.split(" ");
+let reversedName = `${ nameParts[1]} ${nameParts[0]}`;
+console.log(reversedName);
