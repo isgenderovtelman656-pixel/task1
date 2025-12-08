a4c0702c991793f8d6e5a8b81cf38b2aa900078b
telmanisgenderov182-prog <telmanisgenderov182@gmail.com>
2025-12-08 19:09:46 +0400

    ggg

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 // birinci tapsiriq
-let fullName  = "Telman Isgenderov";
-let parts = fullName.split(" ");
-let firstInital = parts[0] [0];
-let lastInital = parts[1] [0];
-console.log(firstInital + lastInital);
+function tarixCevir(tarix){
+    let [il, ay, gun] = tarix.split(".");
 
+    const aylar = {
+        "01":  "yanvar",
+        "02":  "fevral",
+        "03":  "mart",
+        "04":  "aprel",
+        "05":  "may",
+        "06":  "iyun",
+        "07":  "iyul",
+        "08":  "avqust",
+        "09":  "sentyabr",
+        "10":  "oktyabr",
+        "11":  "noyabr",
+        "12":  "dekabr"
+    };
+    return `${Number(gun)} ${aylar[ay]} ${il}-ci il`;
+}
+console.log(tarixCevir("2025.12.08"));
 
-// ikinci taapsiriq
-let fullName1 = "Telman Isgenderov";
-let nameParts = fullName1.split(" ");
-let reversedName = `${ nameParts[1]} ${nameParts[0]}`;
-console.log(reversedName);
+// ikinci tapsiriq
+let ayNomresi = prompt("Ayin nomresini daxil edin (1-12);");
+  let aylar = [
+    "yanvar",
+    "fevral",
+    "mart",
+    "aprel",
+    "may",
+    "iyun",
+    "iyul",
+    "avqust",
+    "sentyabr",
+    "oktyabr",
+    "noyabr",
+    "dekabr",
+]
+let index = Number(ayNomresi) - 1;
+if (index >= 0 && index < 12){
+    console.log(aylar[index]);
+} else{
+    console.log("Duzgun ay nomresinin daxil edin");
+    
+}
