8634f2e74c603d1d5212ac98a45b46d180e85232
telmanisgenderov182-prog <telmanisgenderov182@gmail.com>
2025-12-15 19:04:37 +0400

    sa

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
 // birinci tapsiriq
-function tarixCevir(tarix){
-    let [il, ay, gun] = tarix.split(".");
-
-    const aylar = {
-        "01":  "yanvar",
-        "02":  "fevral",
-        "03":  "mart",
-        "04":  "aprel",
-        "05":  "may",
-        "06":  "iyun",
-        "07":  "iyul",
-        "08":  "avqust",
-        "09":  "sentyabr",
-        "10":  "oktyabr",
-        "11":  "noyabr",
-        "12":  "dekabr"
-    };
-    return `${Number(gun)} ${aylar[ay]} ${il}-ci il`;
+function isValidNumber(phone) {
+    phone = String(phone);
+    if (phone.startsWith("+7") && phone.length ===12) {
+        return true;
+    }
+    return false;
 }
-console.log(tarixCevir("2025.12.08"));
+console.log(isValidNumber("+71234567890"));
+
 
 // ikinci tapsiriq
-let ayNomresi = prompt("Ayin nomresini daxil edin (1-12);");
-  let aylar = [
-    "yanvar",
-    "fevral",
-    "mart",
-    "aprel",
-    "may",
-    "iyun",
-    "iyul",
-    "avqust",
-    "sentyabr",
-    "oktyabr",
-    "noyabr",
-    "dekabr",
-]
-let index = Number(ayNomresi) - 1;
-if (index >= 0 && index < 12){
-    console.log(aylar[index]);
-} else{
-    console.log("Duzgun ay nomresinin daxil edin");
-    
-}
+const capitalizeWords = t =>
+    t.replace(/\b\w/g, c => c.toUpperCase());
+
+console.log(capitalizeWords("hello my is harry"));
