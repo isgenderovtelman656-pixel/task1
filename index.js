cc9e80d1fb26a2f52c4563fa750e3c7e9801ab48
telmanisgenderov182-prog <telmanisgenderov182@gmail.com>
2026-01-08 19:03:31 +0400

    gg

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-let arr = [1, 2, 3, 4, 5];
-let newArr = [...arr];
-
-newArr.pop();
-let increasedArr = [];
-
-for(let i = 0; i < newArr.length; i++){
-    increasedArr.push(newArr[i] + 2);
+// birinci tapsiriq
+function findThereeLettersWord(arr){
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i].length === 3){
+            return arr[i];
+        }
+    }
 }
-console.log(arr);
-console.log(increasedArr);
+console.log(findThereeLettersWord(["Green", "Red", "Blue"]));
 
+// ikinci tapsiriq
+function countEvensAndOdds(arr){
+    let even = 0;
+    let odd = 0;
+
+    arr.forEach(function (num){
+        if(num % 2 === 0){
+            even++;
+        }else{
+            odd++;
+        }
+    });
+    return{ even, odd };
+}
+console.log(countEvensAndOdds([1, 2, 3, 4, 5, 6]));
