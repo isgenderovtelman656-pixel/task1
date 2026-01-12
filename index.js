4179626d4d98631f1c26ba38cb0bcb4384e9e13c
telmanisgenderov182-prog <telmanisgenderov182@gmail.com>
2026-01-12 18:22:20 +0400

    gg

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-// birinci tapsiriq
-function findThereeLettersWord(arr){
-    for (let i = 0; i < arr.length; i++){
-        if(arr[i].length === 3){
-            return arr[i];
-        }
+// birinci tapsirirq
+let arr = [1, 2, 3, 4, 5, 7, 9];
+
+let sum = 0;
+for (let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 !== 0){
+        sum += arr[i];
     }
 }
-console.log(findThereeLettersWord(["Green", "Red", "Blue"]));
-
-// ikinci tapsiriq
-function countEvensAndOdds(arr){
-    let even = 0;
-    let odd = 0;
-
-    arr.forEach(function (num){
-        if(num % 2 === 0){
-            even++;
-        }else{
-            odd++;
-        }
-    });
-    return{ even, odd };
-}
-console.log(countEvensAndOdds([1, 2, 3, 4, 5, 6]));
+console.log(sum);
